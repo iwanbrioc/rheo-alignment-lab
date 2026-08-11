bbb9a296a4736c3dd89a926c6d4e8527278003d6
Iwan Brioc <160613256+iwanbrioc@users.noreply.github.com>
2026-08-11 16:21:59 +0100

    Bump service worker cache for v0.3.1 UI

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='rheo-v0.3';
+const CACHE='rheo-v0.3.1';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./app-core.js','./app-analysis.js','./app-report.js','./model.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
