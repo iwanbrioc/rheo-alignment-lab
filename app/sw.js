0d31c328f936f937d61b891ebda1b8faedf49340
Iwan Brioc <160613256+iwanbrioc@users.noreply.github.com>
2026-08-19 12:22:54 +0100

    Refresh service-worker cache for Realtime voice

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='rheo-v0.3.1-voice1';
+const CACHE='rheo-v0.3.1-realtime-voice1';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./app-core.js','./app-analysis.js','./app-report.js','./model.js','./voice.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',e=>{
